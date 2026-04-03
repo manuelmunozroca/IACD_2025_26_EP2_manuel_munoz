0af71fe8980e0ab7115f287a71ca98f5bf45661e
maneloco <manelmr2015@gmail.com>
2026-04-03 18:19:09 +0100

    Creado el archivo Generation con dos clases. Como quiero dividir lo máximo posible para no liarme con el código el propio individuo es el que se muta a si mismo (no sé si es la mejor manera, iré probando). Los individuos también tienen una función para reproducirse (el individuo que la llama es la madre) La clase generación guarda los individuos y devuelva una lista con los n primeros mejores padres.

--- Generation.py
+++ Generation.py
@@ -1,9 +1,9 @@
-import Cities
+from Cities import City, Data
 import numpy as np
 from typing import List
 
 class Individual:
-    def __init__(self, path = List[int]):
+    def __init__(self, path: List[int]):
         self.path = path
         self.distance = 0.0
 
@@ -11,27 +11,62 @@ class Individual:
         if self.distance == 0.0:
             distance = 0.0
             size = len(self.path)
-            for i in range(size):
+            for i in range(size - 1):
                 from_city = data.cities[self.path[i]]
                 to_city = data.cities[self.path[i+1]]
-                distance += from_city.distance(to_city)
+                distance += from_city.distance_to(to_city)
             self.distance = distance
-            return self.distance
+        return self.distance
 
     def mutate(self, mutation_rate: float) -> None:
         size = len(self.path)
-        for i in range(size):
+        for i in range(size - 1):
             if np.random.rand() < mutation_rate:
-                mutation = np.random.randint(size)
+                mutation = np.random.randint(1, size - 1)
                 self.path[i], self.path[mutation] = self.path[mutation], self.path[i]
+        if self.path[0] != self.path[-1]:
+            self.path[-1] = self.path[0]
+
+    def reproduce(self, father: 'Individual') -> 'Individual':
+        size = len(self.path)
+        mother_path, father_path = self.path.copy(), father.path.copy()
+        start = np.random.randint(1, size//2)
+        child_path = [-1] * size
+        child_path[start:start+size//2] = mother_path[start:start + size//2]
+        child_index, father_index = 0, 0
+        while child_index != size - 1:
+            if child_path[child_index] == -1:
+                while father_path[father_index] in child_path:
+                    father_index += 1
+                child_path[child_index] = father_path[father_index]
+            child_index += 1
+        child_path[-1] = child_path[0]
+        return Individual(child_path)
 
 class Generation:
     def __init__(self, individuals: List[Individual]):
         self.population = individuals
+        self.size = len(individuals)
 
     def get_best_distance(self):
-        return max(self.individuals, key = lambda individual: individual.distance)
+        return min(self.population, key = lambda individual: individual.distance)
 
     def select_best_parents(self, parent_size: int) -> List[Individual]:
         sorted_population = sorted(self.population, key = lambda individual: individual.distance)
         return sorted_population[:parent_size]
+
+    @classmethod
+    def generate_random_generation(cls, data: 'Data', size: int) -> 'Generation':
+        population = list()
+        num_cities = len(data.cities.keys())
+        for individual in range(size):
+            path = list()
+            while len(path) < num_cities:
+                city = np.random.randint(1, num_cities + 1)
+                if city not in path:
+                    path.append(city)
+            path.append(path[0])
+            population.append(Individual(path))
+        return Generation(population)
+
+
